f4c812f42bb008fd98c80c08a5619355c729ed77
Parimal_A Data_Analyst <parimalautade24@gmail.com>
2024-09-19 10:33:17 +0530

    Update 3.Business Problems & Answers3.sql

--- 2.POSTGRE_SQL_PRACTICE2024/3.Business Problems & Answers3.sql
+++ 2.POSTGRE_SQL_PRACTICE2024/3.Business Problems & Answers3.sql
@@ -1,43 +1,39 @@
--- II. INTERVIEW QUESTIONS AND ANSWER PART 3 :
--- -- Customer Queries: TABLE3 :--
---Q.1 Count the number of customers in each city.
-SELECT *FROM customers;
-SELECT city, COUNT(custid) AS customer_count
-FROM customers
-GROUP BY city;
+-- II INTERVIEW QUESTIONS AND ANSWERPART 2 :
+-- products TABLE2 :
+SELECT *FROM products;
+-- Q1.Calculate total number of product we have in products table.
+SELECT COUNT(productid) as totalprods
+from products;  -- 5
 
--- Q.2 Identify customers who have made transactions using PayPal. join use
-SELECT * FROM transactions;
-
-SELECT DISTINCT c.custid, c.cust_name ,t.payment_method
-FROM customers as c
-JOIN transactions as t 
-ON c.custid = t.custid
-WHERE t.payment_method = 'PayPal';
-
--- Q.3 Calculate the total amount spent by each customer.
-SELECT custid, SUM(amount) AS total_spent
-FROM transactions
-GROUP BY custid;
-
--- Q.4 Update the email of customer with ID 204 to 'new_email@example.com'.
-UPDATE customers 
-SET email='new_email@example.com'
-WHERE custid= 204;
-SELECT *FROM customers 
-where custid=204;
--- Q.5 Delete customers who have not made any transactions.
--- Using subquery 
-DELETE FROM customers
-WHERE custid NOT IN (SELECT custid FROM transactions);
--- this query results in DELETE 0, it means that all customers have made transactions, 
--- so there were no customers to delete.
-DELETE FROM customers
-WHERE custid IN (
-    SELECT c.custid
-    FROM customers c
-    LEFT JOIN transactions t ON c.custid = t.custid
-    WHERE t.custid IS NULL
-);
+-- Q2. Identify the products in stock for each category 
+SELECT category, 
+       productname
+FROM products
+WHERE quantity_in_stock > 0
+ORDER BY category;
 
 
+-- Q3.Find the total value of products in stock for each category
+SELECT category ,
+       SUM(price * quantity_in_stock) AS total_value
+FROM products
+GROUP BY category;
+
+-- Q4.  Update the price of all products in the 'Clothing' category to 10% higher
+UPDATE products 
+SET price=price*1.10
+WHERE category ='Clothing';
+
+-- Q5. Delete products with a price less than 50.
+DELETE FROM products
+WHERE price < 50;
+-- TO CHECK
+SELECT *FROM products
+WHERE price>50;
+
+
+-- Q.6 Calculate the total sale amount for each product.
+SELECT *FROM products; 
+SELECT productid, productname, SUM(price * quantity_in_stock) AS total_sales
+FROM products
+GROUP BY productid, productname;
